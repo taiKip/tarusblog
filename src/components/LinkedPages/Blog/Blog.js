8b969a56222fd4635a8a6ebcad9a7b8ec5e8d987
teetarus <victortarus6@gmail.com>
2020-04-30 13:25:59 +0300

    background color changes according to days of the week:data sorted before bieng presented to the UI

--- src/components/LinkedPages/Blog/Blog.js
+++ src/components/LinkedPages/Blog/Blog.js
@@ -1,36 +1,53 @@
-import React,{ Component } from "react";
+import React,{useEffect, useState } from "react";
 import BlogPost from './BlogPost/BlogPost'
 import classes from './Blog.css'
 import axios from 'axios'
-class Blog extends Component{
-    state={
-        blogPosts:''
-    }
-componentDidMount(){
-    axios.get('https://personalblog-39684.firebaseio.com/WeekLyWorkout.json')
-.then(response=>{
-   
-   this.setState=({blogPosts:response.data})
+import Spinner from "../../../UI/Spinner/Spinner";
+import Modal from "../../../UI/Modal/Modal";
 
-}).catch(error=>
-    console.log(error))
+const DAYSOFTHEWEEK=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+function Blog() {
+const[posts,setPosts]=useState([])
+const [showModal,setModal]=useState(false)
+    useEffect(()=>{
+        axios.get('https://personalblog-39684.firebaseio.com/WeekLyWorkout.json')
+        .then(response=>{
+            setPosts(response.data)
+          
+        }).catch(err=>{
+            console.log(err);
+            
+        })
+    },[])
+const sortDays=(a,b)=>{
+    a=DAYSOFTHEWEEK.indexOf(a);
+    b=DAYSOFTHEWEEK.indexOf(b);
+    return a>b ? 1:-1
 }
-render(){
+    let blogPosts= <Spinner/>
     
-  let posts =<p>Something went wrong</p>
-   
-  posts =  Object.keys(this.state.blogPosts).map(day=>{
-      return <BlogPost day={day}/>
-  })
-   
-  
+    if(posts){
+       const days=Object.keys(posts).sort(sortDays)
+       console.log(days[0])
+      blogPosts= days.map((day,i)=>{
+        
+          return <BlogPost
+           key={day +i} 
  
-    return(
+           index={i} day={day}/>
+      })
+    }
+    
+    return (
         <section className={classes.Blog}>
-{posts}
+          
+            <h2 style={{color:"#D9AA8F",
+            fontFamily:'inherit'}}>WeekLy Workout Routine</h2>
+            
+{blogPosts}
         </section>
     )
 }
-}
 
-export default Blog;
+export default Blog
+
--- src/components/LinkedPages/Blog/BlogPost/BlogPost.js
+++ src/components/LinkedPages/Blog/BlogPost/BlogPost.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import classes from './BlogPost.css'
 
-const blogPost =(props)=>(
-    <article className={classes.BlogPost}>
+const blogPost =(props)=>{
+  let exactDay = new Date().getDay();
+
+console.log(exactDay)
+  let assignedClasses = [classes.BlogPost]
+  if(exactDay===(props.index)){
+assignedClasses.push(classes.LightGreen)
+  }
+  return(
+    <article className={assignedClasses.join(' ')}>
     
       <h1>{props.day}
       </h1>
      
        <div>
-          {props.title}
+     {props.title}
         </div>
     </article>
 )
-
+  }
 export default blogPost;
